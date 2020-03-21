429f312c9fd047eb12e1105bf4bf30414255d9d9
Diogoamorim27 <diogoamorim27@gmail.com>
2020-03-21 11:28:56 -0300

    Initial Commit
    Rudimentary system implemented
    bugs fixed courtesy of @joaorabello

---
+++ Assets/MoveFeet.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveFeet : MonoBehaviour
+{
+    PlayerControls controls;
+
+    public Transform leftFoot;
+    public Transform rightFoot;
+
+    public float movementRange = 1f;
+    public float moveSpeed = 0.1f;
+
+    Vector3 originalLeftPos;
+    Vector3 originalRightPos;
+
+    Vector2 leftInput;
+    Vector2 rightInput;
+
+    Vector3 leftInput3d = new Vector3();
+    Vector3 rightInput3d = new Vector3();
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Gameplay.RightStick.performed += ctx => rightInput = ctx.ReadValue<Vector2>();
+        controls.Gameplay.RightStick.canceled += ctx => rightInput = Vector2.zero;
+
+        controls.Gameplay.LeftStick.performed += ctx => leftInput = ctx.ReadValue<Vector2>();
+        controls.Gameplay.LeftStick.canceled += ctx => leftInput = Vector2.zero;
+    }
+    
+    void Start()
+    {
+        originalLeftPos = leftFoot.position;
+        originalRightPos = rightFoot.position;
+    }
+    
+    void Update()
+    {
+        leftInput3d.x = leftInput.x;
+        leftInput3d.y = leftInput.y;
+        rightInput3d.x = rightInput.x;
+        rightInput3d.y = rightInput.y;
+
+    }
+
+    void FixedUpdate()
+    {
+        leftFoot.position = Vector3.Lerp(leftFoot.position, originalLeftPos + leftInput3d * movementRange, moveSpeed);
+        rightFoot.position = Vector3.Lerp(rightFoot.position, originalRightPos + rightInput3d * movementRange, moveSpeed);
+    }
+
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+}
---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,161 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""916c5892-2e06-46ad-8916-29dea635857d"",
+            ""actions"": [
+                {
+                    ""name"": ""LeftStick"",
+                    ""type"": ""Button"",
+                    ""id"": ""216fb92e-6624-442a-b075-85cc60616456"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""RightStick"",
+                    ""type"": ""Button"",
+                    ""id"": ""860f5e34-0e28-4469-9148-ab57cad60261"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""8375f266-10d9-4ca7-aa14-a2740db72dcd"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""LeftStick"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8b7bcc34-d7e6-4e6e-b677-2bb3ee5e5d1a"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""RightStick"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_LeftStick = m_Gameplay.FindAction("LeftStick", throwIfNotFound: true);
+        m_Gameplay_RightStick = m_Gameplay.FindAction("RightStick", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_LeftStick;
+    private readonly InputAction m_Gameplay_RightStick;
+    public struct GameplayActions
+    {
+        private @PlayerControls m_Wrapper;
+        public GameplayActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @LeftStick => m_Wrapper.m_Gameplay_LeftStick;
+        public InputAction @RightStick => m_Wrapper.m_Gameplay_RightStick;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @LeftStick.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLeftStick;
+                @LeftStick.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLeftStick;
+                @LeftStick.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLeftStick;
+                @RightStick.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRightStick;
+                @RightStick.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRightStick;
+                @RightStick.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRightStick;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @LeftStick.started += instance.OnLeftStick;
+                @LeftStick.performed += instance.OnLeftStick;
+                @LeftStick.canceled += instance.OnLeftStick;
+                @RightStick.started += instance.OnRightStick;
+                @RightStick.performed += instance.OnRightStick;
+                @RightStick.canceled += instance.OnRightStick;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnLeftStick(InputAction.CallbackContext context);
+        void OnRightStick(InputAction.CallbackContext context);
+    }
+}
